5b1c1c5378dc161fe800caa4485bce52070ec10c
DerikCarroll <derikcarroll@gmail.com>
2019-10-01 07:50:37 -0500

    Add functions for calculator app.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,25 @@
-// Enter a description of this program here.
+// Simple arithmetic calculator app.
 
 /**
- * Write some pseudo code steps here.
- * Use those steps as your `commit` messages (and delete them from here as you complete a step.)
+ * 1. Add functions for calculator app.
  */
+
+function add(a, b) {
+  return Number(a) + Number(b);
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function remainder(a, b) {
+  return a % b;
+}
